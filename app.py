316137e1e7a6c292ed0fdf5419d42c52a4b93560
MRT <mrt.kousha@gmail.com>
2024-08-06 07:16:26 +0330

    test

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
 import os
-
+import socket
+from contextlib import closing
+from flask import request
+   
+def check_socket(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex((host, port)) == 0:
+            return ("is open")
+        else:
+            return ("is not open")
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! (MRT)"
+    host = request.args.get('host')
+    port = request.args.get('port')
+    result = check_socket(host, int(port))
+    return "Hello World! (MRT)\n {}:{} {}".format(host, port, result)
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
-
     app.run(port=port,host='0.0.0.0')
---
+++ test.py
@@ -0,0 +1,17 @@
+import socket
+from contextlib import closing
+   
+def check_socket(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        if sock.connect_ex((host, port)) == 0:
+            print("Port is open")
+        else:
+            print("Port is not open")
+
+
+
+
+
+host="10.192.133.50"
+port=222
+check_socket(host, port)
